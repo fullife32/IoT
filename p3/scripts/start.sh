5380237e9f48cba75e5bd055dac379e7eed954c8
Eithan Assouline <eithan.assouline6@gmail.com>
2023-03-23 20:08:09 +0100

    Update start.sh

--- p3/scripts/start.sh
+++ p3/scripts/start.sh
@@ -5,7 +5,7 @@ k3d cluster create argocd
 # k3d cluster create argocd -p "8888:8888@loadbalancer"
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl apply -n argocd -f ../confs/argocd/application.yaml
+kubectl apply -n argocd -f ../confs/argocd/wil-application.yaml
 # kubectl -n argocd apply -f ingress/argocd-ingress.yaml
 # kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 # kubectl -n argocd port-forward service/argocd-server 8080:80
