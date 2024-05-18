2a0e039b244546cc6c9a550d50e5ef5f9b1633a1
Administrator <gitlab_admin_00c5d4@example.com>
2024-05-18 16:45:40 +0200

    delete ingress argocd and change to background portforward

--- p3/scripts/start.sh
+++ p3/scripts/start.sh
@@ -8,10 +8,9 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 kubectl wait --for=condition=available deployments --all -n argocd --timeout 300s
 
 kubectl apply -n argocd -f ../confs/argocd/wil-application.yaml
-# kubectl apply -n argocd -f ../ingress/argocd-ingress.yml
 
 echo "argocd login: admin, password:"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-echo "\nTo access ArgoCD UI: kubectl -n argocd port-forward service/argocd-server 4443:443"
+echo "\nTo access ArgoCD UI: kubectl -n argocd port-forward service/argocd-server 4443:443 &"
 
 echo "To access wil-playground app on localhost:8888: kubectl -n dev port-forward service/wil-service 8888:8888"
