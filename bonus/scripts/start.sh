6903be53274dbfb696727f77801f0e34d32d3d39
Administrator <gitlab_admin_00c5d4@example.com>
2024-05-18 17:26:49 +0200

    add manifest for bonus and auto add project to gitlab

--- bonus/scripts/start.sh
+++ bonus/scripts/start.sh
@@ -1,19 +1,30 @@
+GITLAB_ADDR="192.168.43.48"
+
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
 
 helm upgrade --install gitlab gitlab/gitlab -n gitlab --create-namespace \
   -f ../chart/gitlab.yml \
-  --set global.hosts.domain="192.168.43.48.nip.io" \
-  --set global.hosts.externalIP="192.168.43.48" \
+  --set global.hosts.domain="$GITLAB_ADDR.nip.io" \
+  --set global.hosts.externalIP="$GITLAB_ADDR" \
   --set global.edition=ce \
   --set global.hosts.https=false \
   --timeout 600s
 
 kubectl wait --for=condition=available deployments --all -n gitlab --timeout 900s
 
-echo "gitlab URL: gitlab.192.168.43.48.nip.io"
+echo "gitlab URL: gitlab.$GITLAB_ADDR.nip.io"
 echo "login: root, password:"
 kubectl -n gitlab get secret gitlab-gitlab-initial-root-password -o jsonpath="{.data.password}" | base64 -d
+
+read touche
+
+git clone https://github.com/fullife32/eassouli-iot.git
+cd eassouli-iot
+git remote set-url origin http://gitlab.$GITLAB_ADDR.nip.io/root/wil-application.git
+git push --set-upstream origin main
+
+kubectl apply -n argocd -f ../confs/wil-application.yaml
 
 # Install kubectl, k3d, docker and helm automatic for 42
 # Change wil app to run on 80
