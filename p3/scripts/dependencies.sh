8be8264b727ad02b70fed9d0ce43aeb470a9cc0c
fullife32 <eithan.assouline6@gmail.com>
2024-05-23 00:35:45 +0200

    sudo et ajout dependances vm

--- p3/scripts/dependencies.sh
+++ p3/scripts/dependencies.sh
@@ -8,6 +8,10 @@ curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 #kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+sudo chmod 700 get_helm.sh
+./get_helm.sh
 
 #install virtualbox
 sudo apt update && sudo apt install vagrant virtualbox -y
