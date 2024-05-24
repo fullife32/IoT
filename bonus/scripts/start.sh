7191e2a7997721a2dadfb3900718588a194aa13f
fullife32 <eithan.assouline6@gmail.com>
2024-05-25 00:25:19 +0200

    better comments

--- bonus/scripts/start.sh
+++ bonus/scripts/start.sh
@@ -14,18 +14,25 @@ sudo helm upgrade --install gitlab gitlab/gitlab -n gitlab --create-namespace \
 sudo kubectl wait --for=condition=available deployments --all -n gitlab --timeout 900s
 
 echo "\n\nGo to gitlab.$GITLAB_ADDR.nip.io"
-echo "Connect with user : root"
-echo "password : "
+echo "- Connect with user : root"
+echo "  password : "
 sudo kubectl -n gitlab get secret gitlab-gitlab-initial-root-password -o jsonpath="{.data.password}" | base64 -d
-echo "Create a public repository under the name wil-app"
+echo "\n\n- Create a blank project"
+echo "- Project name : wil-app"
+echo "- In project URL, choose the group : root"
+echo "- Visibility Level : Public"
+echo "- Uncheck Initialize repository with a README"
 echo "Press Enter when done to continue"
 
 read touche
 
 git clone https://github.com/fullife32/eassouli-iot.git
 cd eassouli-iot
-git remote set-url origin http://gitlab.$GITLAB_ADDR.nip.io/root/wil-app.git
+git remote remove origin
+git remote add origin http://gitlab.$GITLAB_ADDR.nip.io/root/wil-app.git
 git push --set-upstream origin main
 cd ../
 
 sudo kubectl apply -n argocd -f ./confs/wil-application.yaml
+
+echo "\nTo access wil-playground app : 192.168.56.112:8888\nEnter: sudo kubectl -n dev --address 0.0.0.0 port-forward service/wil-service 8888:8888 &"
--- p3/scripts/start.sh
+++ p3/scripts/start.sh
@@ -12,9 +12,9 @@ sudo kubectl apply -n argocd -f ./confs/wil-application.yaml
 
 sudo kubectl wait --for=condition=available deployments --all -n dev --timeout 300s
 
-echo "\nargocd login: admin, password: "
+echo "\nargocd login: admin\npassword: "
 sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 echo "\n\nTo access ArgoCD UI: 192.168.56.112:4443"
 sudo kubectl -n argocd --address 0.0.0.0 port-forward service/argocd-server 4443:443 &
 
-echo "\nTo access wil-playground app : 192.168.56.112:8888\nEnter: sudo kubectl -n dev --address 0.0.0.0 port-forward service/wil-service 8888:8888 &
+echo "\nTo access wil-playground app : 192.168.56.112:8888\nEnter: sudo kubectl -n dev --address 0.0.0.0 port-forward service/wil-service 8888:8888 &"
